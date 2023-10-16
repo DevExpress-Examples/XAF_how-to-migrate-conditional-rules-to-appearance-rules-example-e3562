80679d811dcfd9334ccaa4713f2d9f5391afcf9d
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-16 19:46:18 +0400

    Change example status to out of maintenance

--- CS/RuleConversion2.Module/Module.cs
+++
@@ -1,9 +0,0 @@
-using DevExpress.ExpressApp;
-
-namespace RuleConversion2.Module {
-    public sealed partial class RuleConversion2Module : ModuleBase {
-        public RuleConversion2Module() {
-            InitializeComponent();
-        }
-    }
-}
